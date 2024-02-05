2e10620598f0a73e7b32925380ab4c96b664e125
imanewkm <imanewkm02@gmail.com>
2024-02-05 14:44:22 -0800

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""sub class based on the base_geometry"""
+
+
+class BaseGeometry:
+    """Represent base geometry"""
+
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")
