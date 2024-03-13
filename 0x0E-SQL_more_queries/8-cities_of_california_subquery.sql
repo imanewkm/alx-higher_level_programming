fb0c0a0220133d548f73709de8d7fe71f4d3542d
imanewkm <imanewkm02@gmail.com>
2024-03-13 15:55:57 -0700

    8-cities_of_california_subquery.sql

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- Search in a database.
+Use hbtn_0d_usa;
+
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY cities.id ASC;
