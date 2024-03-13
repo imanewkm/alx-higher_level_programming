3505b72a0cdb24ae84e4e283209fb502b059db7f
imanewkm <imanewkm02@gmail.com>
2024-03-13 15:51:58 -0700

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,16 @@
--- Create database and table in the database
+-- Create database.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
-	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	state_id INT UNIQUE NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES states(id)
+
+CREATE TABLE IF NOT EXISTS states (
+    id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS cities (
+    id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
