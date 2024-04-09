ebd022540e7f9c17a59b2978f61ebb3e541cd646
imanewkm <imanewkm02@gmail.com>
2024-04-09 15:52:42 -0700

    5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,10 @@
-#!/bin/usr/node
+#!/usr/bin/node
 const Rectangle = require('./4-rectangle');
+
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
 }
+
 module.exports = Square;
